8b558658ee40d21034d57e00b153730c4de4cebf
seanzhao2021 <93017060+seanzhao2021@users.noreply.github.com>
2026-04-26 19:46:14 -0700

    Time: 1392 ms (15.76%), Space: 56.4 MB (5.03%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -6,28 +6,26 @@ class Solution(object):
         :rtype: int
         """
         
-        
+
         dp = {}
 
         def dfs(total):
             #base case:
             #subtracting coin from our current total ends with less than 0 --> fail
             #ends with 0 --> pass
-
             
             if total == 0:
                 return 0
+            
+            if total in dp:
+                return dp[total]
 
             current_best = float("inf")
 
             #for each choice in coins list
             for coin in coins:
                 if total - coin >= 0:
-                    if total - coin in dp:
-                        temp = 1 + dp[total - coin]
-                    else:
-                        temp = 1 + dfs(total - coin)
-                    current_best = min(temp, current_best)
+                    current_best = min(1 + dfs(total - coin), current_best)
 
             dp[total] = current_best
 
@@ -37,5 +35,5 @@ class Solution(object):
 
         if ans == float("inf"):
             return - 1
-        
-        return ans
+        else:
+            return ans
