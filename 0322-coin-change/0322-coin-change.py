3e532c82b7eb0f60f88bd75b24a6112abeb170ea
seanzhao2021 <93017060+seanzhao2021@users.noreply.github.com>
2026-04-26 19:50:17 -0700

    Time: 339 ms (99.48%), Space: 12.6 MB (99%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -7,33 +7,56 @@ class Solution(object):
         """
         
 
-        dp = {}
+        # dp = {}
 
-        def dfs(total):
-            #base case:
-            #subtracting coin from our current total ends with less than 0 --> fail
-            #ends with 0 --> pass
+        # def dfs(total):
+        #     #base case:
+        #     #subtracting coin from our current total ends with less than 0 --> fail
+        #     #ends with 0 --> pass
             
-            if total == 0:
-                return 0
+        #     if total == 0:
+        #         return 0
             
-            if total in dp:
-                return dp[total]
+        #     if total in dp:
+        #         return dp[total]
 
-            current_best = float("inf")
+        #     current_best = float("inf")
 
-            #for each choice in coins list
-            for coin in coins:
-                if total - coin >= 0:
-                    current_best = min(1 + dfs(total - coin), current_best)
+        #     #for each choice in coins list
+        #     for coin in coins:
+        #         if total - coin >= 0:
+        #             current_best = min(1 + dfs(total - coin), current_best)
 
-            dp[total] = current_best
+        #     dp[total] = current_best
 
-            return current_best
+        #     return current_best
         
-        ans = dfs(amount)
+        # ans = dfs(amount)
 
-        if ans == float("inf"):
-            return - 1
-        else:
-            return ans
+        # if ans == float("inf"):
+        #     return - 1
+        # else:
+        #     return ans
+
+        if amount == 0:
+            return 0
+
+        q = deque([0])
+        seen = [False] * (amount + 1)
+        seen[0] = True
+        res = 0
+
+        while q:
+            res += 1
+            for _ in range(len(q)):
+                cur = q.popleft()
+                for coin in coins:
+                    nxt = cur + coin
+                    if nxt == amount:
+                        return res
+                    if nxt > amount or seen[nxt]:
+                        continue
+                    seen[nxt] = True
+                    q.append(nxt)
+
+        return -1
