5b2a6e43584635b7aeab20a8adb7462a5e948f0b
seanzhao2021 <93017060+seanzhao2021@users.noreply.github.com>
2026-05-06 16:23:39 -0700

    Time: 8 ms (96.69%), Space: 12.3 MB (88.21%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -4,19 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        #2 ^ 31
-        power = 2147483648
         res = 0
-
         for i in range(32):
-            if n == 0:
-                continue
-            temp = n % 2
-            if temp == 1:
-                res += (power * temp)
-            n = n / 2
-            power = power / 2
-        
+            bit = (n >> i) & 1
+            res += (bit << (31 - i))
         return res
-
         
